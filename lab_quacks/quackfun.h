c2feef768fe2e43e7875b89f218b16cf3623a597
gzhai5 <78711562+gzhai5@users.noreply.github.com>
2022-08-14 20:46:25 -0400

    Add files via upload

---
+++ lab_quacks/quackfun.h
@@ -0,0 +1,30 @@
+/**
+ * @file quackfun.h
+ * Contains the QuackFun namespace definition.
+ */
+
+#pragma once
+
+#include <stack>
+#include <queue>
+#include<iostream>
+
+/**
+ * Namespace to contain the stack and queue functions for this lab.
+ */
+namespace QuackFun
+{
+    using std::stack;
+    using std::queue;
+
+    template <typename T>
+    T sum(stack<T>& s);
+
+    bool isBalanced(queue<char> input);
+
+    template <typename T>
+    void scramble(queue<T>& q);
+
+};
+
+#include "quackfun.cpp"
