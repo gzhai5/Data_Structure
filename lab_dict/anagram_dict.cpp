8c3fa222b9cdbe217f02192b49886c422b4ebfa1
gzhai5 <78711562+gzhai5@users.noreply.github.com>
2022-08-14 20:42:54 -0400

    Add files via upload

---
+++ lab_dict/anagram_dict.cpp
@@ -0,0 +1,87 @@
+/**
+ * @file anagram_dict.cpp
+ * Implementation of the AnagramDict class.
+ *
+ * @author Matt Joras
+ * @date Winter 2013
+ */
+
+#include "anagram_dict.h"
+
+#include <algorithm> /* I wonder why this is included... */
+#include <fstream>
+
+using std::string;
+using std::vector;
+using std::ifstream;
+
+/**
+ * Constructs an AnagramDict from a filename with newline-separated
+ * words.
+ * @param filename The name of the word list file.
+ */
+AnagramDict::AnagramDict(const string& filename)
+{
+    /* Your code goes here! */
+    string line;
+    ifstream infile(filename);
+
+    if (infile.is_open()){
+        while (getline(infile, line)){
+        	string word = line;
+        	std::sort(word.begin(), word.end());
+        	dict[word].push_back(line);
+        }
+    }
+}
+
+/**
+ * Constructs an AnagramDict from a vector of words.
+ * @param words The vector of strings to be used as source words.
+ */
+AnagramDict::AnagramDict(const vector<string>& words)
+{
+    /* Your code goes here! */
+    for (auto it: words) {
+    	string word = it;
+    	std::sort(word.begin(), word.end());
+    	dict[word].push_back(it);
+    }
+}
+
+/**
+ * @param word The word to find anagrams of.
+ * @return A vector of strings of anagrams of the given word. Empty
+ * vector returned if no anagrams are found or the word is not in the
+ * word list.
+ */
+vector<string> AnagramDict::get_anagrams(const string& word) const
+{
+    /* Your code goes here! */
+    string str = word;
+    std::sort(str.begin(), str.end());
+    if (dict.find(str) == dict.end() || dict.at(str).size() == 1){
+    	return vector<string>();
+    }
+    return dict.at(str);
+    // return vector<string>();
+}
+
+/**
+ * @return A vector of vectors of strings. Each inner vector contains
+ * the "anagram siblings", i.e. words that are anagrams of one another.
+ * NOTE: It is impossible to have one of these vectors have less than
+ * two elements, i.e. words with no anagrams are ommitted.
+ */
+vector<vector<string>> AnagramDict::get_all_anagrams() const
+{
+    /* Your code goes here! */
+    vector<vector<string>> matrix;
+    for (auto it = dict.begin(); it != dict.end(); it++) {
+        if ((it->second).size() > 1){
+            matrix.push_back(it->second);
+        }
+    }
+    return matrix;
+    // return vector<vector<string>>();
+}
---
+++ lab_dict/cartalk_puzzle.cpp
@@ -0,0 +1,43 @@
+/**
+ * @file cartalk_puzzle.cpp
+ * Holds the function which solves a CarTalk puzzler.
+ *
+ * @author Matt Joras
+ * @date Winter 2013
+ */
+
+#include <fstream>
+
+#include "cartalk_puzzle.h"
+
+using namespace std;
+
+/**
+ * Solves the CarTalk puzzler described here:
+ * http://www.cartalk.com/content/wordplay-anyone.
+ * @return A vector of (string, string, string) tuples
+ * Returns an empty vector if no solutions are found.
+ * @param d The PronounceDict to be used to solve the puzzle.
+ * @param word_list_fname The filename of the word list to be used.
+ */
+vector<std::tuple<std::string, std::string, std::string>> cartalk_puzzle(PronounceDict d,
+                                    const string& word_list_fname)
+{
+    vector<std::tuple<std::string, std::string, std::string>> ret;
+
+    /* Your code goes here! */
+    ifstream word_list_(word_list_fname);
+    string word;
+    if (word_list_.is_open()){
+        while (getline(word_list_, word)) {
+            if(word.size() == 5) {
+              string first = word.substr(1);
+              string second = word[0] + word.substr(2);
+              if (d.homophones(word, first) && d.homophones(word, second)) {
+                ret.push_back(tuple<string, string, string>(word, first, second));
+              }
+            }
+        }
+    }
+    return ret;
+}
